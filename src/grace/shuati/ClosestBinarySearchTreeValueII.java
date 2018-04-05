72ea674b890687ff0daf7ad7914e6f3cb59f0e50
Grace <zxr.nju@gmail.com>
2018-04-05 12:04:56 -0700

    add DFS problems

---
+++ src/grace/shuati/ClosestBinarySearchTreeValueII.java
@@ -0,0 +1,13 @@
+package grace.shuati;
+import java.util.*;
+
+/**
+ * Given a non-empty binary search tree and a target value,
+ * find k values in the BST that are closest to the target.
+ */
+
+public class ClosestBinarySearchTreeValueII {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        
+    }
+}
