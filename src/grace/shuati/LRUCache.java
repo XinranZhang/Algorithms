d4a5e83fb162ba89234367ad339c665dc270724a
Grace <zxr.nju@gmail.com>
2018-04-17 20:33:01 -0700

    add queue questions

---
+++ src/grace/shuati/LRUCache.java
@@ -0,0 +1,24 @@
+package grace.shuati;
+
+public class LRUCache {
+    public LRUCache(int capacity) {
+        // do intialization if necessary
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    public int get(int key) {
+        // write your code here
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+        // write your code here
+    }
+}
