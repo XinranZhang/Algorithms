48675bff3163306cfdb1b8f56ad09081d9eceea4
Grace <zxr.nju@gmail.com>
2018-04-18 21:47:27 -0700

    add array questions

--- src/grace/shuati/LRUCache.java
+++ src/grace/shuati/LRUCache.java
@@ -1,8 +1,30 @@
 package grace.shuati;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LRUCache {
+    class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    Node head = new Node(-1, -1);
+    Node tail = new Node(-1, -1);
+    Map<Integer, Node> map = new HashMap<>();
+    int capacity;
     public LRUCache(int capacity) {
-        // do intialization if necessary
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
     }
 
     /*
@@ -10,7 +32,14 @@ public class LRUCache {
      * @return: An integer
      */
     public int get(int key) {
-        // write your code here
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node current = map.get(key);
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+        moveToTail(current);
+        return current.val;
     }
 
     /*
@@ -19,6 +48,25 @@ public class LRUCache {
      * @return: nothing
      */
     public void set(int key, int value) {
-        // write your code here
+        if (get(key) != -1) {
+            map.get(key).val = value;
+            return;
+        }
+        if (map.size() == capacity) {
+            map.remove(head.next.key);
+            head.next = head.next.next;
+            head.next.prev = head;
+        }
+
+        Node node = new Node(key, value);
+        moveToTail(node);
+        map.put(key, node);
+    }
+
+    private void moveToTail(Node current) {
+        current.prev = tail.prev;
+        tail.prev = current;
+        current.prev.next = current;
+        current.next = tail;
     }
 }
---
+++ src/grace/shuati/MedianOfKSortedArrays.java
@@ -0,0 +1,7 @@
+package grace.shuati;
+
+public class MedianOfKSortedArrays {
+    public double findMedian(int[][] nums) {
+        // write your code here
+    }
+}
