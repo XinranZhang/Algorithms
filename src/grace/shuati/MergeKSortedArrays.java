f718520f9fe566f0bd224ef8fc64423a65c9472b
Grace <zxr.nju@gmail.com>
2018-04-19 23:50:49 -0700

    merge sorted arrays

--- src/grace/shuati/MergeKSortedArrays.java
+++ src/grace/shuati/MergeKSortedArrays.java
@@ -6,23 +6,24 @@ import java.util.Queue;
 /**
  * Given k sorted integer arrays, merge them into one sorted array.
  */
+class Element implements Comparable<Element> {
+    int value;
+    int x;
+    int y;
+    public Element(int value, int x, int y) {
+        this.value = value;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Element o) {
+        return this.value - o.value;
+    }
+}
 
 public class MergeKSortedArrays {
-    private class Element implements Comparable<Element> {
-        int value;
-        int x;
-        int y;
-        public Element(int value, int x, int y) {
-            this.value = value;
-            this.x = x;
-            this.y = y;
-        }
 
-        @Override
-        public int compareTo(Element o) {
-            return this.value - o.value;
-        }
-    }
 
     public int[] mergekSortedArrays(int[][] arrays) {
         Queue<Element> pq = new PriorityQueue<>();
