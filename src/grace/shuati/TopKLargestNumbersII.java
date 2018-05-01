443e4d1d890c481ad4c74f92cda552a85dcd3d31
Grace <zxr.nju@gmail.com>
2018-04-30 22:08:45 -0700

    add data structure problems

---
+++ src/grace/shuati/TopKLargestNumbersII.java
@@ -0,0 +1,24 @@
+package grace.shuati;
+import java.util.*;
+
+public class TopKLargestNumbersII {
+    public TopKLargestNumbersII(int k) {
+
+    }
+
+    /*
+     * @param num: Number to be added
+     * @return: nothing
+     */
+    public void add(int num) {
+
+    }
+
+    /*
+     * @return: Top k element
+     */
+    public List<Integer> topk() {
+
+    }
+
+}
