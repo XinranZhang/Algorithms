48eea6fede214d24046c1edaddec3a2c43716128
Grace <zxr.nju@gmail.com>
2018-05-04 22:22:19 -0700

    add data structure

---
+++ src/grace/shuati/TopKFrequentWords.java
@@ -0,0 +1,11 @@
+package grace.shuati;
+
+/**
+ * Given a list of words and an integer k, return the top k frequent words in the list.
+ */
+
+public class TopKFrequentWords {
+    public String[] topKFrequentWords(String[] words, int k) {
+        
+    }
+}
